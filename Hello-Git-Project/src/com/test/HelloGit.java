724398862a9501ba5d44867cc64576a8c43a6b0f
Christopher Dacuya <34495166+cdacuya81@users.noreply.github.com>
2018-02-20 20:31:05 +0800

    Initial Upload

---
+++ Hello-Git-Project/src/com/test/HelloGit.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+
+	}
+
+}
